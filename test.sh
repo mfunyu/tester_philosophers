dab51bcf89f0f8d346c68701b590521905b07ddc
mfunyu <mfunyu@student.42tokyo.jp>
2021-08-27 16:11:00 +0900

    [update] add fork visual test

--- test.sh
+++ test.sh
@@ -7,3 +7,10 @@ for test in ${TESTS[@]}; do
 	file="${DIR_PATH}${test}.sh"
 	$file 2> /dev/null
 done
+
+test=fork_visualizer
+file="${DIR_PATH}${test}.py"
+printf "${CYAN}-------------${test}--------------${RESET}\n"
+for arg in "${ARGS[@]}"; do
+	$PHILO $arg | python3 $file $arg
+done
