9edb84e9a660315bd33c9ebabf552b018e9b7123
mfunyu <mfunyu@student.42tokyo.jp>
2021-09-01 16:28:43 +0900

    [update] take arg

--- test.sh
+++ test.sh
@@ -4,16 +4,27 @@ source ./tests/srcs_sh/const.sh
 
 rm -f ./tests/logs/tester_log
 
-for test in ${TESTS[@]}; do
-	printf "${CYAN}-------------${test}--------------${RESET}\n"
-	file="${SRCS_DIR}/${test}.sh"
-	$file 2> /dev/null
-done
+bash_test (){
+	for test in ${TESTS[@]}; do
+		printf "${CYAN}-------------${test}--------------${RESET}\n"
+		file="${SRCS_DIR}/${test}.sh"
+		$file 2> /dev/null
+	done
+}
 
-test=fork_visualizer
-file="${DIR_PATH}${test}.py"
-printf "${CYAN}-------------${test}--------------${RESET}\n"
-for arg in "${ARGS[@]}"; do
-	printf "${PROMPT} ${PHILO} ${arg}${RESET}\n"
-	$PHILO $arg | python3 $file $arg
-done
+python_test (){
+	test=fork_visualizer
+	file="${DIR_PATH}${test}.py"
+	printf "${CYAN}-------------${test}--------------${RESET}\n"
+	for arg in "${ARGS[@]}"; do
+		printf "${PROMPT} ${PHILO} ${arg}${RESET}\n"
+		$PHILO $arg | python3 $file $arg
+	done
+}
+
+if [ -e $1 ]; then
+	bash_test
+	python_test
+elif [ "$1" == "fork" ]; then
+	python_test
+fi
