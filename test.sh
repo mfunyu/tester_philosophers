c92e95eaeb31ebf08f1fc3fab73c9cc5df0266a4
mfunyu <mfunyu@student.42tokyo.jp>
2021-09-03 23:34:17 +0900

    [update] for test_retval

--- test.sh
+++ test.sh
@@ -22,9 +22,19 @@ python_test (){
 	done
 }
 
+retval_test (){
+	test=test_retval
+	file="${SRCS_DIR}/${test}.sh"
+	printf "${CYAN}-------------${test}--------------${RESET}\n"
+	$file 2> /dev/null
+}
+
 if [ -e $1 ]; then
 	bash_test
 	python_test
+	retval_test
 elif [ "$1" == "fork" ]; then
 	python_test
+elif [ "$1" == "retval" ]; then
+	retval_test
 fi
